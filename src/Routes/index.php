be99644ee604ff5379b4ce47b8eb0974ec45cdaf
Bryan <bryanlim1337@gmail.com>
2026-01-29 13:09:14 +0700

    feat: add login routes and views

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controllers\LoginController;
 use App\Router;
 
 $router = new Router();
 
 $router->get('/', HomeController::class, 'index');
+$router->get('/login', LoginController::class, 'index');
 
 $router->dispatch();
