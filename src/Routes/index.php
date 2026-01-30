3ad34ec32af241329f651d926a56b0eb8b07f7b9
Bryan <bryanlim1337@gmail.com>
2026-01-30 17:13:22 +0700

    fix: dashboard routes

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -2,11 +2,13 @@
 
 use App\Controllers\HomeController;
 use App\Controllers\AuthController;
+use App\Controllers\DashboardController;
 use App\Router;
 
 $router = new Router();
 
 $router->get('/', HomeController::class, 'index');
+$router->get('/dashboard', DashboardController::class, 'index');
 
 $router->get('/login', AuthController::class, 'showLoginPage');
 $router->post('/login', AuthController::class, 'processLogin');
